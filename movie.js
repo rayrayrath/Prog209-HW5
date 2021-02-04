7848137074a5b255b61dfce34e525f449c2aea98
TaylorJeffery <tijeffery97@gmail.com>
2021-02-04 11:09:09 -0800

    movie constructor function with desired properties and methods as well as test function and test html to verify that everything is working as intended.

---
+++ movie.js
@@ -0,0 +1,31 @@
+function Movie(title, rating) {
+    this.title = title;
+    this.rating = rating;
+    this.isRatingValid = function() {
+        if (rating >= 1 && rating <= 5) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    this.isTitleValid = function() {
+        if (title !== "") {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    this.toString = function() {
+        return title + " " + rating;
+    };
+}
+
+function movieTestFunction () {
+    let title = document.getElementById("movieTitle").value;
+    let rating = document.getElementById("movieRating").value;
+    let myMovie = new Movie(title, rating);
+
+    document.getElementById("testArea").innerHTML = myMovie.toString();
+    document.getElementById("isTitleValid").innerHTML = myMovie.isTitleValid();
+    document.getElementById("isRatingValid").innerHTML = myMovie.isRatingValid();
+}
